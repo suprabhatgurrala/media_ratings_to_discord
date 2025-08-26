8c4bab5813acc26fabd6eefc150269588c803de0
Suprabhat Gurrala <supragurrala@gmail.com>
2025-08-26 09:08:44 -0700

    upgrade to uv

--- letterboxd.py
+++ letterboxd.py
@@ -25,35 +25,37 @@ def letterboxd_to_webhook(entries: list):
     num_lists = 0
     num_films = 0
     for entry in entries:
-        id_str = entry.get("id", '')
+        id_str = entry.get("id", "")
 
         if "letterboxd-watch" in id_str or "letterboxd-review" in id_str:
             embeds.append(film_to_embed(entry))
             num_films += 1
-        elif "letterboxd-list" in id_str:
-            embeds.append(list_to_embed(entry))
-            num_lists += 1
+        # elif "letterboxd-list" in id_str:
+        #     embeds.append(list_to_embed(entry))
+        #     num_lists += 1
 
-    author = entries[0]['author']
+    author = entries[0]["author"]
     verb = ""
-    
+
     if num_films == 1:
         movie_plural = "movie"
     else:
         movie_plural = "movies"
-    
+
     if num_lists == 1:
         list_plural = "list"
     else:
         list_plural = "lists"
-    
+
     if num_lists > 0 and num_films > 0:
-        verb = f"logged {num_films} {movie_plural} and created {num_lists} {list_plural}"
+        verb = (
+            f"logged {num_films} {movie_plural} and created {num_lists} {list_plural}"
+        )
     elif num_films > 0:
         verb = f"logged {num_films} {movie_plural}"
     elif num_lists > 0:
         verb = f"created {num_lists} {list_plural}"
-    
+
     webhook_obj = {"content": f"{author} {verb} on Letterboxd:", "embeds": embeds}
 
     return webhook_obj
@@ -63,18 +65,18 @@ def film_to_embed(entry):
     """
     Convert a film entry into an embed to be used in a webhook
     """
-    if entry.get('letterboxd_rewatch') == "Yes":
+    if entry.get("letterboxd_rewatch") == "Yes":
         watched_verbs = "Rewatched"
     else:
         watched_verbs = "Watched"
 
     stars = ""
-    if entry.get('letterboxd_memberrating') is not None:
-        stars = entry.get('title').split("-")[-1].strip().split(" ")[0]
+    if entry.get("letterboxd_memberrating") is not None:
+        stars = entry.get("title").split("-")[-1].strip().split(" ")[0]
 
-    soup = bs4.BeautifulSoup(entry['summary'], 'html.parser')
+    soup = bs4.BeautifulSoup(entry["summary"], "html.parser")
 
-    date_or_review = soup.find_all('p')[-1].text
+    date_or_review = soup.find_all("p")[-1].text
 
     if "Watched on" not in date_or_review:
         review = date_or_review
@@ -85,13 +87,13 @@ def film_to_embed(entry):
 
     date_str = ""
 
-    if entry.get('letterboxd_watcheddate'):
-        watched_date = time.strptime(entry['letterboxd_watcheddate'], "%Y-%m-%d")
+    if entry.get("letterboxd_watcheddate"):
+        watched_date = time.strptime(entry["letterboxd_watcheddate"], "%Y-%m-%d")
 
-        day_of_week = time.strftime('%A', watched_date)
-        month = time.strftime('%B', watched_date)
-        day_of_month = int(time.strftime('%d', watched_date))
-        year = int(time.strftime('%Y', watched_date))
+        day_of_week = time.strftime("%A", watched_date)
+        month = time.strftime("%B", watched_date)
+        day_of_month = int(time.strftime("%d", watched_date))
+        year = int(time.strftime("%Y", watched_date))
 
         date_str = f"{day_of_week} {month} {day_of_month}, {year}"
 
@@ -100,25 +102,21 @@ def film_to_embed(entry):
     description = f"{watched_verbs} {entry['letterboxd_filmtitle']} ({entry['letterboxd_filmyear']}) on {date_str}"
 
     embed_entry = {
-        "title": entry['title'],
+        "title": entry["title"],
         "description": description,
         "url": entry["link"],
         "image": {"url": image_url},
-        "timestamp": pd.to_datetime(entry['published']).tz_convert("UTC").strftime("%Y-%m-%dT%H:%M:%SZ")
+        "timestamp": pd.to_datetime(entry["published"])
+        .tz_convert("UTC")
+        .strftime("%Y-%m-%dT%H:%M:%SZ"),
     }
 
     fields = []
 
     if stars:
-        fields.append({
-            "name": "Rating",
-            "value": stars
-        })
+        fields.append({"name": "Rating", "value": stars})
     if review:
-        fields.append({
-            "name": "Review",
-            "value": f"> {review}"
-        })
+        fields.append({"name": "Review", "value": f"> {review}"})
 
     embed_entry["fields"] = fields
     return embed_entry
@@ -128,9 +126,9 @@ def list_to_embed(list_entry):
     """
     Convert a list entry into an embed to be used in a webhook
     """
-    soup = bs4.BeautifulSoup(list_entry['summary'], 'html.parser')
+    soup = bs4.BeautifulSoup(list_entry["summary"], "html.parser")
 
-    list_description_p = soup.find_all('p', recursive=False)
+    list_description_p = soup.find_all("p", recursive=False)
     list_description = ""
     list_overflow = ""
     if len(list_description_p) == 1:
@@ -141,14 +139,18 @@ def list_to_embed(list_entry):
     elif len(list_description_p) == 2:
         list_description = list_description_p[0].text
         list_overflow = list_description_p[1].text
-        
+
     if list_overflow:
-        overflow = int(list_overflow.replace("...plus ", "").replace(" more. View the full list on Letterboxd.", ""))
+        overflow = int(
+            list_overflow.replace("...plus ", "").replace(
+                " more. View the full list on Letterboxd.", ""
+            )
+        )
     else:
         overflow = 0
-        
-    ol_tags = soup.find_all('ol', recursive=False)
-    ul_tags = soup.find_all('ul', recursive=False)
+
+    ol_tags = soup.find_all("ol", recursive=False)
+    ul_tags = soup.find_all("ul", recursive=False)
 
     fields = []
 
@@ -159,15 +161,15 @@ def list_to_embed(list_entry):
     list_tags = None
 
     if len(ol_tags) > 0:
-        list_tags = soup.ol.find_all('li')
+        list_tags = soup.ol.find_all("li")
         ordered = True
 
     elif len(ul_tags) > 0:
-        list_tags = soup.ul.find_all('li')
+        list_tags = soup.ul.find_all("li")
 
     for i, item in enumerate(list_tags):
-        item_description = ''
-        for para in item.find_all('p'):
+        item_description = ""
+        for para in item.find_all("p"):
             item_description += para.prettify()
 
         item_desc_text = text_maker.handle(item_description)
@@ -177,7 +179,7 @@ def list_to_embed(list_entry):
 
         if len(item_desc_text.strip()) == 0:
             # value cannot be empty
-            tmdb_id = get_tmdb_from_letterboxd(item.a['href'])
+            tmdb_id = get_tmdb_from_letterboxd(item.a["href"])
             item_desc_text = tmdb.fetch_director(tmdb_id)
 
         if ordered:
@@ -185,28 +187,36 @@ def list_to_embed(list_entry):
         else:
             number = ""
 
-        fields.append({
-            "name": f"{number}{item.a.text}",
-            "value": item_desc_text,
-        })
-    
+        fields.append(
+            {
+                "name": f"{number}{item.a.text}",
+                "value": item_desc_text,
+            }
+        )
+
     if overflow > 0:
-        fields.append({
-            "name": f"{list_overflow.replace('View the full list on Letterboxd.', '')}",
-            "value": f"[View the full list on Letterboxd.]({list_entry['link']})"
-        })
+        fields.append(
+            {
+                "name": f"{list_overflow.replace('View the full list on Letterboxd.', '')}",
+                "value": f"[View the full list on Letterboxd.]({list_entry['link']})",
+            }
+        )
         # Subtract 1 from overflow since we've added an extra field
         overflow -= 1
 
-    list_description = f"List of {len(fields) + overflow} movies:\n\n{list_description}".strip()
+    list_description = (
+        f"List of {len(fields) + overflow} movies:\n\n{list_description}".strip()
+    )
 
     embed_entry = {
-            "title": list_entry['title'],
-            "description": list_description,
-            "url": list_entry["link"],
-            "fields": fields,
-            "timestamp": pd.to_datetime(list_entry['published']).tz_convert("UTC").strftime("%Y-%m-%dT%H:%M:%SZ")
-        }
+        "title": list_entry["title"],
+        "description": list_description,
+        "url": list_entry["link"],
+        "fields": fields,
+        "timestamp": pd.to_datetime(list_entry["published"])
+        .tz_convert("UTC")
+        .strftime("%Y-%m-%dT%H:%M:%SZ"),
+    }
 
     return embed_entry
 
@@ -224,6 +234,6 @@ def get_tmdb_from_letterboxd(letterboxd_url):
     """
     raw_html = requests.get(letterboxd_url).text
     parsed_html = bs4.BeautifulSoup(raw_html, "html.parser")
-    tmdb_url = parsed_html.find('a', attrs={'data-track-action': "TMDb"})['href']
-    tmdb_id = tmdb_url.split("/")[-2]
+    tmdb_url = parsed_html.find("a", attrs={"data-track-action": "TMDB"})
+    tmdb_id = tmdb_url["href"].split("/")[-2]
     return tmdb_id
--- trakt.py
+++ trakt.py
@@ -16,50 +16,39 @@ def rating_to_webhook(user_slug, entries):
     num_episodes = 0
 
     for i, entry in enumerate(entries):
-        item_type = entry['type']
+        item_type = entry["type"]
 
         show_title = f"{entry['show']['title']}"
 
-        fields = [{
-            "name": "Show",
-            "value": entry['show']['title']},
-            {
-                "name": "Year",
-                "value": f"{entry['show']['year']}"
-            }
+        fields = [
+            {"name": "Show", "value": entry["show"]["title"]},
+            {"name": "Year", "value": f"{entry['show']['year']}"},
         ]
 
         if item_type == "show":
             num_shows += 1
         elif item_type == "season":
             show_title = f"Season {entry['season']['number']} of {show_title}"
-            fields.append({
-                "name": "Season",
-                "value": f"{entry['season']['number']}"
-            })
+            fields.append({"name": "Season", "value": f"{entry['season']['number']}"})
             num_seasons += 1
         elif item_type == "episode":
             show_title = f"Season {entry['episode']['season']}, Ep. {entry['episode']['number']} of {show_title}"
-            fields.append({
-                "name": "Season",
-                "value": f"{entry['episode']['season']}"
-            })
-            fields.append({
-                "name": "Episode",
-                "value": f"{entry['episode']['number']} - '{entry['episode']['title']}'"
-            })
+            fields.append({"name": "Season", "value": f"{entry['episode']['season']}"})
+            fields.append(
+                {
+                    "name": "Episode",
+                    "value": f"{entry['episode']['number']} - '{entry['episode']['title']}'",
+                }
+            )
             num_episodes += 1
 
         if i > 5:
             # Don't include more than 5 embeds per webhook
             continue
 
-        rating = entry['rating']
+        rating = entry["rating"]
 
-        fields.append({
-            "name": "Rating",
-            "value": f"{rating}/10"
-        })
+        fields.append({"name": "Rating", "value": f"{rating}/10"})
 
         description = f"{user_slug} rated {show_title} **{rating}/10** on Trakt.tv"
 
@@ -67,14 +56,16 @@ def rating_to_webhook(user_slug, entries):
 
         image_url = tmdb.fetch_poster(tmdb_id)
 
-        embeds.append({
-            "title": show_title,
-            "description": description,
-            # "url": f"https://trakt.tv/users/{user_slug}/ratings",
-            "image": {"url": image_url},
-            "fields": fields,
-            "timestamp": entry['rated_at']
-        })
+        embeds.append(
+            {
+                "title": show_title,
+                "description": description,
+                # "url": f"https://trakt.tv/users/{user_slug}/ratings",
+                "image": {"url": image_url},
+                "fields": fields,
+                "timestamp": entry["rated_at"],
+            }
+        )
 
     shows = ""
     if num_shows == 1:
@@ -105,6 +96,9 @@ def rating_to_webhook(user_slug, entries):
     else:
         item_str = f"{shows}{seasons}{episodes}"
 
-    webhook_obj = {"content": f"{user_slug} rated {item_str} on Trakt.tv:", "embeds": embeds}
+    webhook_obj = {
+        "content": f"{user_slug} rated {item_str} on Trakt.tv:",
+        "embeds": embeds,
+    }
 
     return webhook_obj
