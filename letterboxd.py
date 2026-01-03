3379e11124bb14751a6c8195cb254fa9d5461b25
Suprabhat Gurrala <supragurrala@gmail.com>
2026-01-02 20:45:18 -0800

    fix lists

--- letterboxd.py
+++ letterboxd.py
@@ -1,11 +1,8 @@
 import time
 
 import bs4
-import html2text
 import pandas as pd
-import requests
-
-import tmdb
+from html_to_markdown import convert
 
 
 def letterboxd_to_webhook(entries: list):
@@ -30,9 +27,9 @@ def letterboxd_to_webhook(entries: list):
         if "letterboxd-watch" in id_str or "letterboxd-review" in id_str:
             embeds.append(film_to_embed(entry))
             num_films += 1
-        # elif "letterboxd-list" in id_str:
-        #     embeds.append(list_to_embed(entry))
-        #     num_lists += 1
+        elif "letterboxd-list" in id_str:
+            embeds.append(list_to_embed(entry))
+            num_lists += 1
 
     author = entries[0]["author"]
     verb = ""
@@ -126,114 +123,14 @@ def list_to_embed(list_entry):
     """
     Convert a list entry into an embed to be used in a webhook
     """
-    soup = bs4.BeautifulSoup(list_entry["summary"], "html.parser")
-
-    list_description_p = soup.find_all("p", recursive=False)
-    list_description = ""
-    list_overflow = ""
-    if len(list_description_p) == 1:
-        if "...plus" in list_description_p[0].text:
-            list_overflow = list_description_p[0].text
-        else:
-            list_description = list_description_p[0].text
-    elif len(list_description_p) == 2:
-        list_description = list_description_p[0].text
-        list_overflow = list_description_p[1].text
-
-    if list_overflow:
-        overflow = int(
-            list_overflow.replace("...plus ", "").replace(
-                " more. View the full list on Letterboxd.", ""
-            )
-        )
-    else:
-        overflow = 0
-
-    ol_tags = soup.find_all("ol", recursive=False)
-    ul_tags = soup.find_all("ul", recursive=False)
-
-    fields = []
-
-    text_maker = html2text.HTML2Text()
-    text_maker.ignore_links = True
-
-    ordered = False
-    list_tags = None
-
-    if len(ol_tags) > 0:
-        list_tags = soup.ol.find_all("li")
-        ordered = True
-
-    elif len(ul_tags) > 0:
-        list_tags = soup.ul.find_all("li")
-
-    for i, item in enumerate(list_tags):
-        item_description = ""
-        for para in item.find_all("p"):
-            item_description += para.prettify()
-
-        item_desc_text = text_maker.handle(item_description)
-
-        if len(item_desc_text) > 1024:
-            item_desc_text = item_desc_text[0:1021] + "..."
-
-        if len(item_desc_text.strip()) == 0:
-            # value cannot be empty
-            tmdb_id = get_tmdb_from_letterboxd(item.a["href"])
-            item_desc_text = tmdb.fetch_director(tmdb_id)
-
-        if ordered:
-            number = f"{i + 1}. "
-        else:
-            number = ""
-
-        fields.append(
-            {
-                "name": f"{number}{item.a.text}",
-                "value": item_desc_text,
-            }
-        )
-
-    if overflow > 0:
-        fields.append(
-            {
-                "name": f"{list_overflow.replace('View the full list on Letterboxd.', '')}",
-                "value": f"[View the full list on Letterboxd.]({list_entry['link']})",
-            }
-        )
-        # Subtract 1 from overflow since we've added an extra field
-        overflow -= 1
-
-    list_description = (
-        f"List of {len(fields) + overflow} movies:\n\n{list_description}".strip()
-    )
 
     embed_entry = {
         "title": list_entry["title"],
-        "description": list_description,
+        "description": convert(list_entry["description"]),
         "url": list_entry["link"],
-        "fields": fields,
         "timestamp": pd.to_datetime(list_entry["published"])
         .tz_convert("UTC")
         .strftime("%Y-%m-%dT%H:%M:%SZ"),
     }
 
     return embed_entry
-
-
-def get_tmdb_from_letterboxd(letterboxd_url):
-    """
-    Scrape a Letterboxd movie page to get it's TMDb link
-    Parameters
-    ----------
-    letterboxd_url: URL to a film page on Letterboxd
-
-    Returns
-    -------
-    URL to the film on TheMovieDB
-    """
-    raw_html = requests.get(letterboxd_url).text
-    parsed_html = bs4.BeautifulSoup(raw_html, "html.parser")
-    tmdb_url = parsed_html.find("a", attrs={"data-track-action": "TMDB"})
-    tmdb_id = tmdb_url["href"].split("/")[-2]
-    return tmdb_id
